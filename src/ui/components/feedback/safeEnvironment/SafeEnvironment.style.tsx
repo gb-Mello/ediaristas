5231d1cd3a258a8234bb02f5ecff3e3674bfcfd2
gb-Mello <gabrielmello633@hotmail.com>
2022-02-23 23:49:00 -0300

    finalizando-Header

--- src/ui/components/feedback/safeEnvironment/SafeEnvironment.style.tsx
+++ src/ui/components/feedback/safeEnvironment/SafeEnvironment.style.tsx
@@ -1,7 +1,8 @@
 import { styled } from "@mui/material/styles";
 
 export const SafeEnvironmentContainer = styled("div")`
-  background-color: ${({ theme }) => theme.palette.text.secondary};
+  color: ${({ theme }) => theme.palette.text.secondary};
+  background-color: ${({ theme }) => theme.palette.background.default};
   text-align: right;
   padding: ${({ theme }) => theme.spacing(2)} 0;
   font-size: 12px;
